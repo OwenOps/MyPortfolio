d77b21a2727156ec9fecddcad7be233e1bbe1b83
Owen <rebellerowen@gmail.com>
2024-02-02 18:46:45 +0100

    sauv1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,27 @@ import { FooterComponent } from './component/footer/footer.component';
 
 //-------PrimeNG-------//
 import { ButtonModule } from 'primeng/button';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { EducationComponent } from './component/education/education.component';
+import { ExperiencesComponent } from './component/experiences/experiences.component';
+import { WorkComponent } from './component/work/work.component';
+import { BlogComponent } from './component/blog/blog.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    EducationComponent,
+    ExperiencesComponent,
+    WorkComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
--- src/app/component/side-bar/side-bar.component.ts
+++ src/app/component/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild  } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,9 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent {
+  @ViewChild('mainContent') mainContent!: ElementRef;
   activeNavItem: string = '';
+
+  ngAfterViewInit() {
+    console.log(this.mainContent);
+  }
 
   setActiveNavItem(navItem: string) {
     this.activeNavItem = navItem;
+    // console.log(this.mainContent)
+
+    // const element = this.mainContent.nativeElement.querySelector(`#${navItem}`);
+    // if (element) {
+    //   element.scrollIntoView({ behavior: 'smooth' });
+    // }
   }
 }
